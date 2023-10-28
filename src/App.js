335dfe6937bfe3baca3c7f3c9ebf0fe271f4cb1e
Anushka7310 <anushkaguptably@gmail.com>
2023-10-28 16:23:30 +0530

    added Navbar component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import CharacterList from "./components/pages/CharacterList";
 import CharacterDetails from "./components/pages/CharacterDetails";
 import Favourites from "./components/pages/Favourites";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<CharacterList />} />
           <Route path="/character/:id" exact element={<CharacterDetails />} />
