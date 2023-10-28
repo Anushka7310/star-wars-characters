c8b4623ff22a32ae9e71f09cc722aa718f14c599
Anushka7310 <anushkaguptably@gmail.com>
2023-10-28 16:20:54 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import CharacterList from "./components/pages/CharacterList";
+import CharacterDetails from "./components/pages/CharacterDetails";
+import Favourites from "./components/pages/Favourites";
 
 function App() {
   return (
     <ChakraProvider>
-      <h1>Hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<CharacterList />} />
+          <Route path="/character/:id" exact element={<CharacterDetails />} />
+          <Route path="/favourites" exact element={<Favourites />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
