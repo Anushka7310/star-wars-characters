c50298507a0ff9045bbf7d5c745ea264567596a7
Anushka7310 <anushkaguptably@gmail.com>
2023-10-28 18:36:43 +0530

    Added black theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import { ChakraProvider, CSSReset, Box, position } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CharacterList from "./components/pages/CharacterList";
 import CharacterDetails from "./components/pages/CharacterDetails";
 import Favourites from "./components/pages/Favourites";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: `black`, // Set the background color to black
+        color: "white", // Set the text color to white
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<CharacterList />} />
-          <Route path="/character/:id" exact element={<CharacterDetails />} />
-          <Route path="/favourites" exact element={<Favourites />} />
-        </Routes>
-      </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Router>
+        <NavBar />
+        <div style={{ marginTop: "64px", padding: "20px" }}>
+          <Routes>
+            <Route path="/character/:id" element={<CharacterDetails />} />
+            <Route path="/favorites" element={<Favourites />} />
+            <Route
+              path="/"
+              element={
+                <Box p={4}>
+                  <CharacterList />
+                </Box>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </ChakraProvider>
   );
 }
